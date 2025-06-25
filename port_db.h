373ab956481babb0cef3969c265c3ce3f4cab2e0
John31415 <johnmauris04@gmail.com>
2025-06-24 22:39:44 -0400

    algo 2

--- port_db.h
+++
@@ -1,8 +0,0 @@
-#ifndef PORT_DB_H
-#define PORT_DB_H
-
-void load_json(const char *filename);
-char *verificar_puerto(int puerto);
-void liberar_memoria();
-
-#endif
--- port_services.h
+++
@@ -1,6 +0,0 @@
-#ifndef PORT_SERVICES_H
-#define PORT_SERVICES_H
-
-const char* get_service_name(int port);
-
-#endif
